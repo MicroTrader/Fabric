3cb5e1400a29385b2f97aacf894e25a970df8790
Suminda Sirinath Salpitikorala Dharmasena <sirnath1978m@gmail.com>
2016-09-11 10:20:01 +0530

    .

--- src/main/java/com/sakrio/fabric/GrowableIndex.java
+++ src/main/java/com/sakrio/fabric/GrowableIndex.java
@@ -110,7 +110,7 @@ import com.sakrio.collections.arrays.LongGrowable;
 /**
  * Created by sirinath on 08/09/2016.
  */
-public final class IndexGrowable extends LongGrowable implements Indexed {
+public final class GrowableIndex extends LongGrowable implements Indexed {
     @Override
     public final IndexType getIndexType() {
         return IndexType.growable;
